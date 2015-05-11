81842e4bcbd4916b039cd44ee326763310a483af
Toni Verbeiren <toni.verbeiren@gmail.com>
2015-05-11 12:35:19 +0200

    Enable webcomponents in notebook

--- zeppelin-web/Gruntfile.js
+++ zeppelin-web/Gruntfile.js
@@ -310,7 +310,8 @@ module.exports = function (grunt) {
             'images/*',
             'fonts/*',
             'WEB-INF/*',
-            'scripts/ace/{,*/}/{,*/}/*'
+            'scripts/ace/{,*/}/{,*/}/*',
+            'bower_components/{,*/}/{,*/}/*'
           ]
         }, {
           expand : true,
